41a82a77e9fa68306911366cc48444543557f0bd
NITHUEDITX848960 <kingharoldassbad@gmail.com>
2025-09-16 18:34:29 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,5 +8,5 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || ''
+    SESSION_ID: process.env.SESSION_ID || 'YASIYA-MD~MIhnSbYQ#DoRDET7G2d5iJ7zOXZNZGZnMS1QmanMnO9-erKc1FPw'
 };
